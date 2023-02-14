53f149555780aea150ee50c280ff535e587f5d37
Alex Sheperd <alexsheperd@protonmail.com>
2023-02-14 09:53:58 -0600

    added nathan's changes

---
+++ go-standard-lib/httpExamples.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var key = []byte("ThisIsAKeyThatMustBe32Characters")
+
+func HttpGetExample() {
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		log.Panicf("http.Get failed with %s", err.Error())
+	}
+	defer resp.Body.Close()
+	msg, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("io.ReadAll failed with %s", err.Error())
+	}
+	fmt.Println(string(msg))
+}
+
+func HttpPostExample() {
+	name := strings.NewReader("Gopher")
+	resp, err := http.Post("http://localhost:8081/thing", "text/plain", name)
+	if err != nil {
+		log.Panicf("http.Post failed with %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	msg, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("io.ReadAll failed with %s", err.Error())
+	}
+	fmt.Println("POST Status code: ", resp.Status, " | Message: ", string(msg))
+}
+
+func HttpServerExamples() {
+	server := http.NewServeMux()
+	server.HandleFunc("/file", fileHandler)
+	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("Success!"))
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	err := http.ListenAndServe("localhost:8080", server)
+	if err != nil {
+		log.Fatalf("ListenAndServer error: %s", err.Error())
+	}
+}
+
+func s() string {
+	return "hi"
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		status, resp := handleReadFile("secretfile.aes")
+		writeResponse(status, resp, w)
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			writeResponse(500, []byte("There was an error"), w)
+		}
+
+		exStruct := JsonUnmarshal[ExampleStruct](body) // To verify structural compliance
+		encrypted := EncryptAES(key, JsonMarshal(*exStruct))
+		WriteFileContents("secretfile.aes", []byte(encrypted+"\n"))
+	}
+}
+
+func handleReadFile(name string) (int, []byte) {
+	data := ReadFileContents(name)
+	var objects []*ExampleStruct
+
+	for _, line := range strings.Split(string(data), "\n") {
+		if len(line) < 1 {
+			continue
+		}
+		decrypted := DecryptAES(key, []byte(line))
+		p := JsonUnmarshal[ExampleStruct]([]byte(decrypted))
+		objects = append(objects, p)
+	}
+
+	result := JsonMarshal(objects)
+
+	return 200, result
+}
+
+func writeResponse(status int, resp []byte, w http.ResponseWriter) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	i, err := w.Write(resp)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Wrote %d bytes to response", i)
+}
